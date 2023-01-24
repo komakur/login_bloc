bee82d6a3861535b1a867ce6c242ea580c93e31c
kostvick <my.mail.kostya@gmail.com>
2023-01-24 20:39:51 +0200

    add views

---
+++ lib/authentication/authentication.dart
@@ -0,0 +1,1 @@
+export 'package:flutter_bloc_login/authentication/bloc/authentication_bloc.dart';
---
+++ lib/login/login.dart
@@ -0,0 +1,2 @@
+export 'package:flutter_bloc_login/login/view/login_page.dart';
+export 'package:flutter_bloc_login/login/view/login_form.dart';
